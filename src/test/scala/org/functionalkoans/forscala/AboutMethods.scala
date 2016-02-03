ceabf8331e4a03460f44e206ab8236d2c1b92c87
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-03 11:07:10 +0000

    Finished exercise 'AboutMethods'

--- src/test/scala/org/functionalkoans/forscala/AboutMethods.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutMethods.scala
@@ -13,7 +13,7 @@ class AboutMethods extends KoanSuite {
     def add(x: Int, y: Int) = {
       x + y
     }
-    add(6, 7) should be(__)
+    add(6, 7) should be(13)
   }
 
   koan(
@@ -23,7 +23,7 @@ class AboutMethods extends KoanSuite {
       //Notice the :Int at the end of the method
       x + y
     }
-    add(2, 10) should be(__)
+    add(2, 10) should be(12)
   }
 
 
@@ -39,14 +39,14 @@ class AboutMethods extends KoanSuite {
       else x + y
     }
 
-    add(2, 10) should be(__)
-    add(1, 1) should be(__)
+    add(2, 10) should be(12)
+    add(1, 1) should be(2)
   }
 
 
   koan( """If a method does not of have equal it is considered `Unit` which is analogous to `void` in Java""") {
     def foo(x: Int) { //Note: No `=`
-      (x + 4) should be(__)
+      (x + 4) should be(9)
     }
     foo(5)
   }
@@ -55,19 +55,19 @@ class AboutMethods extends KoanSuite {
     """If you want to have an = on the method, while still explicitly returning Unit you can make the return type `Unit`,
       | this also analogous to `void""") {
     def foo(x: Int): Unit = { //Note we are declaring Unit
-      (x + 4) should be(__)
+      (x + 4) should be(7)
     }
     foo(3)
   }
 
   koan( """Once you have an =, it is understood that there will be a return type and can be inferred""") {
     def foo(x: Int) = 3 + 4
-    foo(3).isInstanceOf[Int] should be(__) //.isInstanceOf[...] is analogous to Java's instanceOf
+    foo(3).isInstanceOf[Int] should be(true) //.isInstanceOf[...] is analogous to Java's instanceOf
   }
 
   koan( """Of course if you wish to be explicit about the return type, you can attach it at the end of the method""") {
     def foo(x: Int): Int = 3 + 4
-    foo(3).isInstanceOf[Int] should be(__)
+    foo(3).isInstanceOf[Int] should be(true)
   }
 
   koan(
@@ -79,7 +79,7 @@ class AboutMethods extends KoanSuite {
       else x * factorial(x-1)
     }
 
-    factorial(4) should be(__) // List(...) is how a list is created more about lists later.
+    factorial(4) should be(24) // List(...) is how a list is created more about lists later.
 
     //Note: Fire up a REPL and paste factorial(100000)!
   }
@@ -104,7 +104,7 @@ class AboutMethods extends KoanSuite {
       fact(i, 1)
     }
 
-    factorial(4) should be(__)
+    factorial(4) should be(24)
 
     //Note: Fire up a REPL and try factorial(100000) now!
   }
@@ -133,7 +133,7 @@ class AboutMethods extends KoanSuite {
 
     class Pennies(val n:Int)
     def doYouHaveAnySpareChange_?() = new Pennies(25)
-    doYouHaveAnySpareChange_?.n should be(__)
+    doYouHaveAnySpareChange_?.n should be(25)
   }
 
   koan(
@@ -148,7 +148,7 @@ class AboutMethods extends KoanSuite {
     }
 
     val probationEmployee = `put employee on probation`(new Employee("Milton", "Waddams", ""))
-    probationEmployee.`employee status` should be (__)
+    probationEmployee.`employee status` should be ("Probation")
   }
 
   koan(
@@ -158,7 +158,7 @@ class AboutMethods extends KoanSuite {
     def add(a:Int, b:Int) = a + b //implied return type of Int!
     def performSideEffect():Unit = System.currentTimeMillis
 
-    add(4,6) should be (__)
+    add(4,6) should be (10)
     performSideEffect() //Notice the parenthesis, since the method we called is Unit!
   }
 
@@ -171,7 +171,7 @@ class AboutMethods extends KoanSuite {
     }
 
     val foo = new Foo(9)
-    10 ~: foo should be (__)
-    foo.~:(40) should be (__)
+    10 ~: foo should be (22)
+    foo.~:(40) should be (52)
   }
 }
