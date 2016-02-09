100f26c81aec064136e11b50dcca9d34feff96fa
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-09 14:32:32 +0000

    Finished exercise 'AboutFormatting'

--- src/test/scala/org/functionalkoans/forscala/AboutFormatting.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutFormatting.scala
@@ -7,7 +7,7 @@ class AboutFormatting extends KoanSuite with ShouldMatchers {
 
   koan("String can be placed in format") {
     val s = "Hello World"
-    "Application %s".format(s) should be (__)
+    "Application %s".format(s) should be ("Application Hello World")
   }
 
   koan("Character Literals can be an a single character") {
@@ -18,8 +18,8 @@ class AboutFormatting extends KoanSuite with ShouldMatchers {
     //format(a) is a string format, meaning the "%c".format(x)
     //will return the string representation of the char.
 
-    "%c".format(a) should be(__)
-    "%c".format(b) should be(__)
+    "%c".format(a) should be("a")
+    "%c".format(b) should be("B")
 
   }
 
@@ -31,22 +31,22 @@ class AboutFormatting extends KoanSuite with ShouldMatchers {
     val f = '\\'
 
 
-    "%c".format(c) should be(__)
-    "%c".format(d) should be(__)
-    "%c".format(e) should be(__)
-    "%c".format(f) should be(__)
+    "%c".format(c) should be("a")
+    "%c".format(d) should be("a")
+    "%c".format(e) should be("\"")
+    "%c".format(f) should be("\\")
   }
 
 
   koan("Formatting can also include numbers") {
     val j = 190
-    "%d bottles of beer on the wall" format j - 100 should be (__)
+    "%d bottles of beer on the wall" format j - 100 should be ("90 bottles of beer on the wall")
   }
 
   koan("Formatting can be used for any number of items, like a string and a number") {
     val j = 190
     val k = "vodka"
 
-    "%d bottles of %s on the wall".format(j - 100, k) should be (__)
+    "%d bottles of %s on the wall".format(j - 100, k) should be ("90 bottles of vodka on the wall")
   }
 }
